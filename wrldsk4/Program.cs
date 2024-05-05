c55eeabe2ba6a06c2beace853b2ed4764b41eb83
nastyakanteeva <117898518+nastyakanteeva@users.noreply.github.com>
2024-05-05 14:24:39 +0300

    Add files via upload

---
+++ wrldsk4/Program.cs
@@ -0,0 +1,26 @@
+﻿using reader;
+using student;
+using System.Reflection.PortableExecutable;
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[3];
+        students[0] = new("Иванов И.И.", "01", 19);
+        students[1] = new("Петров П.П.", "33", 20);
+        students[2] = new("Алёшин А.А.", "22", 22);
+
+        Reader[] readers = new Reader[3];
+        readers[0] = new("Иванов И.И.", 3425, "Иформационные технологии", new DateTime(2022, 2, 22), "+89285603545");
+        readers[1] = new("Петров П.П.", 3426, "Иформационные технологии", new DateTime(2022, 2, 23), "+82756420970");
+        readers[2] = new("Алёшин А.А.", 3427, "Иформационные технологии", new DateTime(2022, 2, 22), "+89373562319");
+
+        string[] books = { "Приключения", "Словарь", "Энциклопедия" };
+
+        readers[0].takeBook(books);
+        readers[0].takeBook(students[0].name, 1);
+        readers[1].returnBook(books);
+        readers[1].returnBook(students[1].name, 2);
+    }
+}
---
+++ wrldsk4/SR.cs
@@ -0,0 +1,65 @@
+﻿namespace student
+{
+    class Student
+    {
+        public string name = "Untitled";
+        public string group = "Untitled";
+        private int age = 0;
+        public Student(string name, string group, int age)
+        {
+            this.name = name;
+            this.group = group;
+            this.age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{name}, Группа: {group},Возраст{age}");
+        }
+    }
+}
+
+namespace reader
+{
+    class Reader
+    {
+        public string name = "Untitled";
+        public int number_library_card = 0;
+        public string faculty = "Untitled";
+        private DateTime date_of_birth;
+        public string telephone = "Untitled";
+        public Reader(string name, int number_library_card, string faculty, DateTime date_of_birth, string telephone)
+        {
+            this.name = name;
+            this.number_library_card = number_library_card;
+            this.faculty = faculty;
+            this.date_of_birth = date_of_birth;
+            this.telephone = telephone;
+        }
+        public void takeBook(string name, int number_book)
+        {
+            Console.WriteLine($"{name}, взял {number_book} книг");
+        }
+        public void takeBook(params string[] books)
+        {
+            Console.Write($"{name}, взял книги:");
+            foreach (string book in books)
+            {
+                Console.Write(book + ",");
+            }
+            Console.WriteLine();
+        }
+        public void returnBook(string name, int number_book)
+        {
+            Console.WriteLine($"{name}, вернул {number_book} книг");
+        }
+        public void returnBook(params string[] books)
+        {
+            Console.Write($"{name}, вернул книги:");
+            foreach (string book in books)
+            {
+                Console.Write(book + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
